156135fe8908ea391a6f8f235a0565b1a449f212
Chetan Panara <chetanpanara8@gmail.com>
2024-11-30 19:51:24 +0530

    Update Home.jsx

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -32,7 +32,7 @@ export default function Home() {
     <>
       <div className="container">
         <div className="row mt-5 justify-content-center">
-          <div className="col-md-8">
+          <div className="col-12">
 
             <Link to={'/create'} className='btn btn-primary mb-3'>Add User</Link>
             
